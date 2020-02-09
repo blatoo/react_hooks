801efdc12b93fea54bd9d9ef8f68ad058acbf0c6
Conny Gu <connygy@googlemail.com>
2020-02-09 21:09:41 +0100

    Passing State Data Across Components

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import './IngredientList.css';
 
+/**
+ * 
+ * @param {*} props.ingredients 
+ * @param  props.onRemoveItem <Func>
+ */
 const IngredientList = props => {
   return (
     <section className="ingredient-list">
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
+import IngredentList from "./IngredientList";
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+const Ingredients = () => {
+	const [userIngredients, setUserIngredients] = useState([]);
 
-function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+	const addIngredientHandler = ingredient => {
+		setUserIngredients(prevIngredients => [
+			...prevIngredients,
+			{ id: Math.random().toString(), ...ingredient }
+		]);
+	};
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<IngredientForm onAddIngredient={addIngredientHandler}/>
+
+			<section>
+				<Search />
+				<IngredentList ingredients={userIngredients} onRemoveItem={()=>{}}/>
+			</section>
+		</div>
+	);
+};
 
 export default Ingredients;
