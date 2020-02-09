6d8abb0b63474bb6a655232309839d0c2b7c9e4f
Conny Gu <connygy@googlemail.com>
2020-02-10 00:11:14 +0100

    464. useEffect() & Loading Data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import IngredentList from "./IngredientList";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 
 const Ingredients = () => {
 	const [userIngredients, setUserIngredients] = useState([]);
+
+	useEffect(() => {
+		fetch("https://react-hooks-8efdb.firebaseio.com/ingredients.json")
+			.then(response => response.json())
+			.then(responseData => {
+				const loadedIngredients = [];
+				for (const key in responseData) {
+					loadedIngredients.push({
+						id: key,
+						title: responseData[key].title,
+						amount: responseData[key].amount
+					});
+				}
+				setUserIngredients(loadedIngredients);
+			});
+	}, []);
 
 	const addIngredientHandler = ingredient => {
 		fetch("https://react-hooks-8efdb.firebaseio.com/ingredients.json", {
